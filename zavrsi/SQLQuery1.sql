4563aab92a070bc3fc041f1853decf4e870f9cc6
Bruno <131766847+brunobusic20@users.noreply.github.com>
2023-10-17 20:24:57 +0200

    baza

---
+++ zavrsi/SQLQuery1.sql
@@ -0,0 +1,42 @@
+use master;
+drop database if exists imenik;
+go
+create database imenik;
+go
+use imenik;
+select * from kontakt;
+select * from email;
+
+create table kontakt(
+	sifra int not null primary key identity(1,1),
+	ime varchar(50) not null,
+	prezime varchar(50) not null,
+	broj char(10),
+	adresa varchar(50) not null
+
+)
+
+create table email(
+	sifra int not null primary key identity(1,1),
+	emailadresa varchar(100)
+)
+
+
+alter table email add foreign key (sifra) references kontakt(sifra);
+
+insert into kontakt(ime,prezime,broj,adresa)
+values('Bruno','Buši?','0998474271','Petefija Sandora 16');
+
+insert into email(emailadresa)
+values('brunobusic20@gmail.com');
+
+insert into kontakt(ime,prezime,broj,adresa)
+values
+('Angela','Buši?','0913969853','Petefija Sandora 16'),
+('Gabriela','Buši?','0917655008','Petefija Sandora 16');
+
+insert into email(emailadresa)
+values
+('angelabusic29@gmail.com'),
+('gabrielabusic9@gmail.com');
+
